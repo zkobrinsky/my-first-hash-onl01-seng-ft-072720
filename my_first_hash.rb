c3d74fd1cb39d71e4de6fc1e5a273069ba0123a9
Zach Kobrinsky <zachkobrinsky@gmail.com>
2020-07-31 17:05:02 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,5 +1,5 @@
 def my_hash
-  # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
+  my_household ={"Mom" => "Marina", "Dad" => "Zach", "cat" => "Henry", "baby" => "Max"}
 
   
 end
