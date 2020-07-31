8912bdd06c0e55b266aa296d1e036d0d2fd29a33
Zach Kobrinsky <zachkobrinsky@gmail.com>
2020-07-31 17:08:57 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -29,6 +29,9 @@ def adding
                  }
   # add 2 muskets to the shipping_manifest hash below
 
+  shipping_manifest["muskets" => 2, "gun powder" => 4]
+  
+
 
 
   # add 4 gun powder to the shipping_manifest hash below
