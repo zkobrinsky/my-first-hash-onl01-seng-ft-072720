3da393c514d5e1a6b0c75319bfd7b291bc2bd407
Zach Kobrinsky <zachkobrinsky@gmail.com>
2020-07-31 17:07:49 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -5,9 +5,6 @@ end
 
 def shipping_manifest
 the_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3}
-  # fill that hash with key/value pairs that describe the following information:
-  # We have 5 whale bone corsets, 2 porcelain vases and 3 oil paintings
-
 
 end
 
@@ -17,6 +14,8 @@ def retrieval
                   "porcelain vases" => 2,
                   "oil paintings" => 3
                  }
+
+  shipping_manifest["oil paintings"]
 
   # Look up the value of the "oil paintings" key in the shipping_manifest hash below
 
